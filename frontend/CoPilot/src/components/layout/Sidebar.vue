ee9d8b6ea5508c69e74bffcdf49bf27ece069a3a
Wessel Blokzijl <wesselb94@gmail.com>
2020-05-28 10:55:59 +0200

    Add copilot stuff

---
+++ frontend/CoPilot/src/components/layout/Sidebar.vue
@@ -0,0 +1,64 @@
+<template>
+  <aside class="main-sidebar">
+
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+
+      <!-- Sidebar user panel (optional) -->
+      <div class="user-panel">
+        <div class="pull-left image">
+          <img :src="user.avatar" />
+        </div>
+        <div class="pull-left info">
+          <div>
+            <p class="white">{{user.displayName}}</p>
+          </div>
+          <!-- <a href="javascript:;"> -->
+          <!--   <i class="fa fa-circle text-success"></i> Online -->
+          <!-- </a> -->
+        </div>
+      </div>
+
+      <!-- Sidebar Menu -->
+      <sidebar-menu />
+      <!-- /.sidebar-menu -->
+    </section>
+    <!-- /.sidebar -->
+  </aside>
+</template>
+<script>
+import SidebarMenu from './SidebarMenu'
+
+export default {
+  name: 'Sidebar',
+  props: ['user'],
+  components: { SidebarMenu },
+  mounted: function() {
+    window
+      .jQuery('[data-toggle="hideseek"]')
+      .off()
+      .hideseek()
+  }
+}
+</script>
+<style scope="local">
+.user-panel .image img {
+  border-radius: 50%;
+}
+#searchForm {
+  padding-left: 0em;
+  padding-right: 0em;
+}
+#searchContainer {
+  height: 100%;
+  padding-bottom: 0em;
+}
+#search {
+  width: 80%;
+  float: right;
+}
+
+#search-btn {
+  width: 20%;
+}
+</style>
---
+++ frontend/CoPilot/src/routes.js
@@ -0,0 +1,97 @@
+import DashView from './components/Dash.vue'
+import LoginView from './components/Login.vue'
+import NotFoundView from './components/404.vue'
+
+// Import Views - Dash
+import DashboardView from './components/views/Dashboard.vue'
+
+import AccountView from './components/views/user/Account.vue'
+import TransactionsView from './components/views/user/Transactions.vue'
+
+import BankDashView from './components/views/bank/Dashboard.vue'
+import PolicyView from './components/views/bank/Policy.vue'
+import BankStatsView from './components/views/bank/Statistics.vue'
+
+import RegulatorDashView from './components/views/regulator/Dashboard.vue'
+import CasesView from './components/views/regulator/Cases.vue'
+
+import NotaryDashView from './components/views/notary/Dashboard.vue'
+import RequestsView from './components/views/notary/Requests.vue'
+import NotaryStatsView from './components/views/notary/Statistics.vue'
+
+// Routes
+const routes = [
+  {
+    path: '/login',
+    component: LoginView
+  },
+  {
+    path: '/',
+    component: DashView,
+    children: [
+      {
+        path: 'dashboard',
+        alias: '',
+        component: DashboardView,
+        name: 'Dashboard',
+        meta: {description: 'Overview of environment'}
+      }, {
+        path: 'account',
+        component: AccountView,
+        name: 'Wallet',
+        meta: {description: 'User wallet'}
+      }, {
+        path: 'transactions',
+        component: TransactionsView,
+        name: 'Transactions',
+        meta: {description: 'User transactions'}
+      }, {
+        path: 'bank',
+        component: BankDashView,
+        name: 'Dashboard',
+        meta: {description: 'Simple and advance table in CoPilot'}
+      }, {
+        path: 'bank/policy',
+        component: PolicyView,
+        name: 'Policy',
+        meta: {description: 'Simple and advance table in CoPilot'}
+      }, {
+        path: 'bank/statistics',
+        component: BankStatsView,
+        name: 'Statistics',
+        meta: {description: 'Simple and advance table in CoPilot'}
+      }, {
+        path: 'regulator',
+        component: RegulatorDashView,
+        name: 'Dashboard',
+        meta: {description: 'Simple and advance table in CoPilot'}
+      }, {
+        path: 'regulator/cases',
+        component: CasesView,
+        name: 'Cases',
+        meta: {description: 'Simple and advance table in CoPilot'}
+      }, {
+        path: 'notary',
+        component: NotaryDashView,
+        name: 'Dashboard',
+        meta: {description: 'Simple and advance table in CoPilot'}
+      }, {
+        path: 'notary/requests',
+        component: RequestsView,
+        name: 'Requests',
+        meta: {description: 'Simple and advance table in CoPilot'}
+      }, {
+        path: 'notary/statistics',
+        component: NotaryStatsView,
+        name: 'Statistics',
+        meta: {description: 'Simple and advance table in CoPilot'}
+      }
+    ]
+  }, {
+    // not found handler
+    path: '*',
+    component: NotFoundView
+  }
+]
+
+export default routes
