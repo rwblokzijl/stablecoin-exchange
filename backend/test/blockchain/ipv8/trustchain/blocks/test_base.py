db9736f3080ad297aed8494a16403b39b6339ad3
Wessel Blokzijl <wesselb94@gmail.com>
2021-02-14 16:27:44 +0100

    Add submodule

---
+++ backend/test/blockchain/ipv8/trustchain/blocks/test_base.py
@@ -0,0 +1,17 @@
+import asynctest
+
+from ipv8.test.attestation.trustchain.test_block import TestBlock, MockDatabase
+
+class TestBalance(unittest.TestCase):
+
+    """Test case docstring."""
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_name(self):
+        pass
+
