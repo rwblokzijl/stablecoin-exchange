12f3c2a09fc9c7c3618ad6d5d62acae6a06f0f90
Wessel Blokzijl <wesselb94@gmail.com>
2021-04-15 12:35:15 +0200

    Make eval script checkpoint since gateway variable

--- run_eval.sh
+++ run_eval.sh
@@ -3,7 +3,6 @@
 # export CLIENTS="${1:-2}"
 # export GATEWAYS="${2:-$CLIENTS}"
 export TRANSACTIONS_TO_DO=1024
-export CHECKPOINT_EVERY=32
 
 # if [ "$CLIENTS" -lt "$GATEWAYS" ]; then
 #     echo more gateways than clients...
@@ -12,24 +11,30 @@ export CHECKPOINT_EVERY=32
 
 # echo running evuation with $GATEWAYS GATEWAYS and $CLIENTS CLIENTS
 
-# A=( 2:1 \
-#     2:2)
+# A=( 2:1:32 \
+#     2:2:32 \
+#     4:1:32 \
+#     4:2:32 \
+#     4:4:32 \
+#     8:1:32 \
+#     8:2:32 \
+#     8:4:32 \
+#     8:8:32 )
 
-A=( 2:1 \
-    2:2 \
-    4:1 \
-    4:2 \
-    4:4 \
-    8:1 \
-    8:2 \
-    8:4 \
-    8:8 )
+A=( 4:4:1 \
+    4:4:2 \
+    4:4:4 \
+    4:4:8 \
+    4:4:16)
 
 for a in ${A[@]}
 do
-    export CLIENTS=${a%:*}
+    export CLIENTS=${a%:*:*}
     tmp=${a#"$CLIENTS"}
-    export GATEWAYS=${tmp#:}
+    tmp=${tmp#:}
+    export GATEWAYS=${tmp%:*}
+    tmp=${tmp#"$GATEWAYS"}
+    export CHECKPOINT_EVERY=${tmp#:}
 
     docker system prune -f
     docker volume rm gateway_sync
